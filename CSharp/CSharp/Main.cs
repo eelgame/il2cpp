e6298df70b8601118f2e39003596840d7dab6d74
yangjian <eelgame@qq.com>
2022-04-13 19:50:38 +0800

    il2cpp test

--- CSharp/CSharp/Main.cs
+++ CSharp/CSharp/Main.cs
@@ -7,7 +7,23 @@ namespace CSharp
     {
         public static void Entry()
         {
-            new AutoRun().Execute(Array.Empty<string>());
+            var assembly = typeof(Main).Assembly;
+
+            // var types = assembly.GetTypes();
+            // foreach (var type in types)
+            // {
+            //     foreach (var method in type.GetMethods())
+            //     {
+            //         if (method.GetCustomAttribute<TestAttribute>() != null)
+            //         {
+            //             Console.WriteLine(method.Name);
+            //         }
+            //     }
+            // }
+            //
+            // Console.WriteLine(assembly.FullName);
+
+            new AutoRun(assembly).Execute(Array.Empty<string>());
         }
     }
 }
