fe317575123ea0d2392b0b274360fda1de9fde60
yangjian <eelgame@qq.com>
2022-04-13 11:40:32 +0800

    自动化测试

--- il2cpp_test.bat
+++ il2cpp_test.bat
@@ -2,5 +2,5 @@
 
 .\il2cpp\build\deploy\netcoreapp3.1\il2cpp.exe --convert-to-cpp -emit-null-checks --enable-array-bounds-check --dotnetprofile="unityaot" --generatedcppdir=./output/CSharp --assembly=./output/CSharp-stripped/CSharp.dll --copy-level=None
 
-.\il2cpp\build\deploy\netcoreapp3.1\il2cpp.exe --compile-cpp --libil2cpp-static --configuration=Release --forcerebuild --generate-cmake=il2cpp --dotnetprofile="unityaot" --baselib-directory=.\windowsstandalonesupport\Variations\win64_nondevelopment_il2cpp --platform=WindowsDesktop --architecture=x64 --outputpath=./output/GameAssembly-CSharp-x64/GameAssembly-CSharp-x64.dll --generatedcppdir=./output/CSharp --cachedirectory=./output/GameAssembly-CSharp-x64//cache
+.\il2cpp\build\deploy\netcoreapp3.1\il2cpp.exe --compile-cpp --libil2cpp-static --configuration=Release --forcerebuild --generate-cmake=il2cpp --dotnetprofile="unityaot" --baselib-directory=.\windowsstandalonesupport\Variations\win64_nondevelopment_il2cpp --platform=WindowsDesktop --architecture=x64 --outputpath=./output/GameAssembly-CSharp-x64/GameAssembly-CSharp-x64.dll --generatedcppdir=./output/CSharp --cachedirectory=./output/GameAssembly-CSharp-x64/cache
 pause
