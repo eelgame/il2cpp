ed5b810920f7f5b067cc03b21f80789ff9daa693
yangjian <eelgame@qq.com>
2022-04-13 11:31:49 +0800

    自动化测试

---
+++ CSharp/CSharp/Main.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CSharp
+{
+    public class Main
+    {
+        public static void Hello()
+        {
+            var list = new List<string>();
+            list.Add("Hello");
+            list.Add("World");
+        }
+    }
+}
---
+++ il2cpp_test.bat
@@ -0,0 +1,6 @@
+.\il2cpp\build\deploy\netcoreapp3.1\UnityLinker.exe --out=./output/CSharp-stripped --i18n=none --core-action=link --include-assembly=./dll/CSharp.dll,./dll/mscorlib.dll --dotnetruntime=il2cpp --dotnetprofile=unityaot --use-editor-options
+
+.\il2cpp\build\deploy\netcoreapp3.1\il2cpp.exe --convert-to-cpp -emit-null-checks --enable-array-bounds-check --dotnetprofile="unityaot" --generatedcppdir=./output/CSharp --assembly=./output/CSharp-stripped/CSharp.dll --copy-level=None
+
+.\il2cpp\build\deploy\netcoreapp3.1\il2cpp.exe --compile-cpp --libil2cpp-static --configuration=Release --forcerebuild --generate-cmake=il2cpp --dotnetprofile="unityaot" --baselib-directory=C:\Unity\2020.3.7f1\Editor\Data\PlaybackEngines\windowsstandalonesupport\Variations\win64_nondevelopment_il2cpp --platform=WindowsDesktop --architecture=x64 --outputpath=./output/GameAssembly-CSharp-x64/GameAssembly-CSharp-x64.dll --generatedcppdir=./output/CSharp --cachedirectory=./output/GameAssembly-CSharp-x64//cache
+pause
