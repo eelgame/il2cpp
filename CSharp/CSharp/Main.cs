7d23c1a9cd1d4d098248f2b9d98e1f74186bb0cf
yangjian <eelgame@qq.com>
2022-04-13 14:27:20 +0800

    il2cpp

--- CSharp/CSharp/Main.cs
+++ CSharp/CSharp/Main.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CSharp
 {
     public class Main
     {
-        public static void Hello()
+        public static void Entry()
         {
-            var list = new List<string>();
-            list.Add("Hello");
-            list.Add("World");
+            Console.WriteLine("CSharp.Main.Entry Fix");
         }
     }
 }
