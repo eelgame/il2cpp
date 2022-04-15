ed3c06cdc1ea36ea555c68a6297909589b199529
eelgame <eelgame@qq.com>
2022-04-15 13:32:22 +0800

    shell

---
+++ il2cpp_run_aot.sh
@@ -0,0 +1,5 @@
+#export CSHARP_DLL_PATH=`pwd`/CSharp/CSharp/bin/Debug/CSharp.dll
+cd output/GameAssembly-CSharp-x64/
+rundll32.exe GameAssembly-CSharp-x64.dll,il2cpp_test
+read -s -n 1 -p "Press any key to continue . . ."
+echo ""
