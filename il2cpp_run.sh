4a6eb8ea777a1d8a37bdbd8788df79151555cddf
eelgame <eelgame@qq.com>
2022-04-15 11:07:16 +0800

    脚本

--- il2cpp_run.sh
+++ il2cpp_run.sh
@@ -1,5 +1,5 @@
+export CSHARP_DLL_PATH=`pwd`/CSharp/CSharp/bin/Debug/CSharp.dll
 cd output/GameAssembly-CSharp-x64/
-export CSHARP_DLL_PATH=$(dirname $0)/CSharp/CSharp/bin/Debug/CSharp.dll
 rundll32.exe GameAssembly-CSharp-x64.dll,il2cpp_test
 read -s -n 1 -p "Press any key to continue . . ."
 echo ""
