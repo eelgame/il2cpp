cd43dffeb19097e3e643746aa5ac38caf8ab2099
yangjian <eelgame@qq.com>
2022-04-26 17:15:15 +0800

    Main

--- CSharp/ConsoleApp/Program.cs
+++ CSharp/ConsoleApp/Program.cs
@@ -8,22 +8,7 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            var managedPath = @"C:\Users\18611\Documents\apks\tmp\Tile Match Brain Puzzle Game_v1.8.6_apkpure.com\assets\bin\Data\Managed";
-            var metadataPath = $@"{managedPath}\etc\mono\56fdc7a7d4d85e543b7fb78c901610bd";
-            
-            var bytes = File.ReadAllBytes(metadataPath);
-
-            for (int i = 4; i < bytes.Length; i++)
-            {
-                bytes[i] ^= 0x6E;
-            }
-
-            if (!Directory.Exists(Path.Combine(managedPath, "Metadata")))
-            {
-                Directory.CreateDirectory(Path.Combine(managedPath, "Metadata"));
-            }
-            
-            File.WriteAllBytes(Path.Combine(managedPath, "Metadata", "global-metadata.dat"), bytes);
+            CSharp.Main.Entry();
         }
     }
 }
