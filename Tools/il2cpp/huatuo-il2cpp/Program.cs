fec5e9cd3af1eb4413a676835852e138e50e6ff0
yangjian <eelgame@qq.com>
2022-05-09 15:59:43 +0800

    泛型扫描

---
+++ Tools/il2cpp/huatuo-il2cpp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using il2cpp.Conversion;
+using Mono.Cecil;
+using NiceIO;
+using Unity.IL2CPP.AssemblyConversion.Phases;
+using Unity.IL2CPP.Contexts;
+using Unity.IL2CPP.GenericsCollection;
+
+namespace il2cpp
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Il2CppOptionParser.ParseArguments(args, out var continueToRun, out var exitCode, out var platform,
+                    out var buildingOptions, out var profilerOutput);
+
+                var inputData = ContextDataFactory.CreateConversionDataFromOptions();
+
+                using (var context = AssemblyConversionContext.SetupNew(
+                           inputData,
+                           ContextDataFactory.CreateConversionParametersFromOptions(),
+                           ContextDataFactory.CreateTopLevelDataFromOptions()))
+                {
+                    InitializePhase.Run(context);
+                    SetupPhase.Run(context, context.Results.Initialize.AllAssembliesOrderedByDependency);
+                    var primaryCollectionContext = context.GlobalPrimaryCollectionContext.CreateCollectionContext();
+
+                    var assemblyDefinitions = new List<AssemblyDefinition>();
+                    
+                    foreach (var assemblyDefinition in context.Results.Initialize.AllAssembliesOrderedByDependency)
+                        if (inputData.Assemblies.Contains(new NPath(assemblyDefinition.MainModule.FileName)))
+                            assemblyDefinitions.Add(assemblyDefinition);
+
+                    var inflatedCollectionCollector = GenericsCollector.Collect(primaryCollectionContext,
+                        new ReadOnlyCollection<AssemblyDefinition>(assemblyDefinitions));
+
+                    foreach (var type in inflatedCollectionCollector.AsReadOnly().Types)
+                        if (type.Scope is ModuleDefinition md && !inputData.Assemblies.Contains(new NPath(md.FileName)))
+                            Console.WriteLine(type);
+
+                    return 0;
+                }
+            }
+            catch (Exception arg)
+            {
+                Console.Error.WriteLine($"Unhandled exception: {arg}");
+                return -1;
+            }
+        }
+    }
+}
---
+++ Tools/il2cpp/huatuo-preprocess/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using Mono.Cecil;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        var dlls = new[] {"il2cpp.dll", "Unity.IL2CPP.dll", "Unity.IL2CPP.Building.dll"};
+
+        foreach (var dll in dlls)
+        {
+            var readerParameters = new ReaderParameters();
+            var assemblyResolver = new DefaultAssemblyResolver();
+
+            var dir = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            assemblyResolver.AddSearchDirectory(dir);
+
+            readerParameters.InMemory = true;
+            readerParameters.AssemblyResolver = assemblyResolver;
+
+            var assemblyDefinition =
+                AssemblyDefinition.ReadAssembly(Path.Combine(dir, dll), readerParameters);
+
+            var ca = new CustomAttribute(
+                assemblyDefinition.MainModule.ImportReference(
+                    typeof(InternalsVisibleToAttribute).GetConstructor(new[] {typeof(string)})));
+            ca.ConstructorArguments.Add(new CustomAttributeArgument(assemblyDefinition.MainModule.TypeSystem.String,
+                "huatuo-il2cpp"));
+            assemblyDefinition.CustomAttributes.Add(ca);
+            assemblyDefinition.Write(Path.Combine(dir, assemblyDefinition.MainModule.Name));
+        }
+
+        return 0;
+    }
+}
